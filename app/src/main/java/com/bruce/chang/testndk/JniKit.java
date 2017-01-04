84e7115a75b928e94b9c8783ffa4e385afcddbb4
201216323 <1060140613@qq.com>
2017-01-05 07:56:28 +0800

    1

--- app/src/main/java/com/bruce/chang/testndk/JniKit.java
+++ app/src/main/java/com/bruce/chang/testndk/JniKit.java
@@ -22,3 +22,18 @@ public class JniKit {
      */
     public native String sayHello();
 }
+
+//com.bruce.chang.testndk.JniKit
+//
+//
+//        NDK开发流程：
+//
+//        在java里面写native代码
+//
+//        在main目录下创建jni目录写C代码，--生成头文件
+//
+//        配置动态链接库的名称
+//
+//        加载动态链接库
+//
+//        使用
