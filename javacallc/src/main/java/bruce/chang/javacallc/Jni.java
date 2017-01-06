882c27ee99e6e9e73318f80256906eb92d637711
[201216323] <[1060140613@qq.com]>
2017-01-06 18:47:39 +0800

    jnicallc

---
+++ javacallc/src/main/java/bruce/chang/javacallc/Jni.java
@@ -0,0 +1,13 @@
+package bruce.chang.javacallc;
+
+/**
+ * Created by: BruceChang
+ * Date on : 2017/1/6.
+ * Time on: 10:24
+ * Progect_Name:TestNDK
+ * Source Github：
+ * Description:
+ */
+
+public class Jni {
+}
